a6d157162e946c65a07af85747428ae52de4746d
ShadowMario <44785097+ShadowMario@users.noreply.github.com>
2024-09-29 22:43:20 -0300

    backup 4

--- js/selector.js
+++ js/selector.js
@@ -50,6 +50,7 @@ function createSidebarContent(prefix) {
 		'getTextSize': 'text.html',
 		'getTextFont': 'text.html',
 		'getTextWidth': 'text.html',
+		'luaTextExists': 'text.html',
 
 		// Camera
 		'setCameraScroll': 'camera.html',
@@ -63,6 +64,27 @@ function createSidebarContent(prefix) {
 		'cameraShake': 'camera.html',
 		'cameraFlash': 'camera.html',
 		'cameraFade': 'camera.html',
+
+		// Input
+		'mouseClicked': 'input.html',
+		'mousePressed': 'input.html',
+		'mouseReleased': 'input.html',
+		'getMouseX': 'input.html',
+		'getMouseY': 'input.html',
+		'keyJustPressed': 'input.html',
+		'keyPressed': 'input.html',
+		'keyReleased': 'input.html',
+		'keyboardJustPressed': 'input.html',
+		'keyboardPressed': 'input.html',
+		'keyboardReleased': 'input.html',
+		'anyGamepadJustPressed': 'input.html',
+		'anyGamepadPressed': 'input.html',
+		'anyGamepadReleased': 'input.html',
+		'gamepadJustPressed': 'input.html',
+		'gamepadPressed': 'input.html',
+		'gamepadReleased': 'input.html',
+		'gamepadAnalogX': 'input.html',
+		'gamepadAnalogY': 'input.html',
 
 		// Timer
 		'runTimer': 'timer.html',
@@ -93,7 +115,38 @@ function createSidebarContent(prefix) {
 		'precacheImage': 'precache.html',
 		'precacheSound': 'precache.html',
 		'precacheMusic': 'precache.html',
-		'addCharacterToList': 'precache.html'
+		'addCharacterToList': 'precache.html',
+
+		// Save Data
+		'initSaveData': 'savedata.html',
+		'flushSaveData': 'savedata.html',
+		'eraseSaveData': 'savedata.html',
+		'getDataFromSave': 'savedata.html',
+		'setDataFromSave': 'savedata.html',
+
+		// File I/O
+		'getTextFromFile': 'file.html',
+		'checkFileExists': 'file.html',
+		'saveFile': 'file.html',
+		'deleteFile': 'file.html',
+		'directoryFileList': 'file.html',
+
+		// Script
+		'getRunningScripts': 'script.html',
+		'callScript': 'script.html',
+		'addLuaScript': 'script.html',
+		'addHScript': 'script.html',
+		'removeLuaScript': 'script.html',
+		'removeHScript': 'script.html',
+		'isRunning': 'script.html',
+		'setVar': 'script.html',
+		'getVar': 'script.html',
+		'setOnScripts': 'script.html',
+		'setOnLuas': 'script.html',
+		'setOnHScript': 'script.html',
+		'callOnScripts': 'script.html',
+		'callOnHScript': 'script.html',
+		'callOnLuas': 'script.html'
 	};
 
 	// Create list elements and append to itemList
